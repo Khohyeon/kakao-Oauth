2ae339a47965401ae6a3477c3b19a212bbf88d9d
khh5762 <khh5762@naver.com>
2023-03-16 12:20:55 +0900

    Feart: 카카오 OAUTH 사용하여 로그인 성공

--- src/main/java/shop/mtcoding/getintherelogin/model/User.java
+++ src/main/java/shop/mtcoding/getintherelogin/model/User.java
@@ -1,10 +1,14 @@
 package shop.mtcoding.getintherelogin.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int id;
     private String username;  // ssar@nate.com 이런식으로 나두면 겹쳐서 위험 그래서 kakao_ssar@nate.com 이런식으로 구현
